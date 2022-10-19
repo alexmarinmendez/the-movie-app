8b5b536485de165cee42f1faebc34be084627c9b
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-10-19 17:56:07 -0500

    feat: render the first 20 movies

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const cargarPeliculas = async () => {
     try {
-        const respuesta = await fetch('https://api.themoviedb.org/3/movie/popular?page=3&api_key=b11a474a2fd87941a8936c55c3cd38d7')
+        const respuesta = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=b11a474a2fd87941a8936c55c3cd38d7')
         
         if (respuesta.status === 200) {
             const datos = await respuesta.json()
-            console.log(datos)
+            
+            let peliculas = ''
+            datos.results.forEach(pelicula => {
+                peliculas += `<h1>${pelicula.title}</h1>`
+            })
+            document.getElementById('contenedor').innerHTML = peliculas
+
         } else if (respuesta.status === 401) {
             console.log('You don\'t have access to this resource')
         } else if (respuesta.status === 404) {
