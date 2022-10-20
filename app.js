499d6dc6c84156c73a5be5276c348c0a1bdf19e1
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-10-19 19:13:52 -0500

    feat: axios get query for fetching movies

--- app.js
+++ app.js
@@ -16,10 +16,19 @@ btnAnterior.addEventListener('click', () => {
 
 const cargarPeliculas = async () => {
     try {
-        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?page=${pagina}&language=es-CL&api_key=b11a474a2fd87941a8936c55c3cd38d7`)
-        
+        const respuesta = await axios.get('https://api.themoviedb.org/3/movie/popular', {
+            params: {
+                // api_key: 'b11a474a2fd87941a8936c55c3cd38d7',
+                language: 'es-CL',
+                page: pagina
+            },
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTFhNDc0YTJmZDg3OTQxYTg5MzZjNTVjM2NkMzhkNyIsInN1YiI6IjYzNGZiYTNiMzU4MThmMDA3OTkxZTMzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._2is3YHv2rcrDkYzoSdvxodp_Q8FnnL9WdDONVy7o5s'
+            }
+        })
+
         if (respuesta.status === 200) {
-            const datos = await respuesta.json()
+            const datos = await respuesta.data
 
             console.log(datos)
             
